628a707435cb2aae67cdb1ca04433384ad678e03
morinowww <moreno.chester.eiggerg@gmail.com>
2024-11-20 22:31:51 +0800

    completed initial drawing

--- main.cpp
+++ main.cpp
@@ -274,6 +274,99 @@ GLfloat verticesVBO[] = {
 	0.32f, -0.50f, 0.0f,		// Bottom-right
 	0.22f, -0.50f, 0.0f,		// Bottom-left
 
+	//Tinky-Winky: Head (Antenna)
+	0.55f, 0.55f, 0.0f,
+	0.65f, 0.55f, 0.0f,
+	0.60f, 0.45f, 0.0f,
+	0.60f, 0.45f, 0.0f,
+	0.60f, 0.35f, 0.0f,
+	0.60f, 0.30f, 0.0f,
+	//Tinky-Winky: Head
+    0.70f, 0.30f, 0.0f,    		// 0°
+    0.688f, 0.353f, 0.0f,	  	// 22.5°
+    0.6707f, 0.3707f, 0.0f, 	// 45°
+    0.653f, 0.387f, 0.0f,   	// 67.5°
+    0.60f, 0.40f, 0.0f,     	// 90°
+    0.548f, 0.387f, 0.0f,   	// 112.5°
+    0.5293f, 0.3707f, 0.0f, 	// 135°
+    0.513f, 0.353f, 0.0f,   	// 157.5°
+    0.50f, 0.30f, 0.0f,     	// 180°
+    0.513f, 0.247f, 0.0f,   	// 202.5°
+    0.5293f, 0.2293f, 0.0f, 	// 225°
+    0.548f, 0.213f, 0.0f,   	// 247.5°
+    0.60f, 0.20f, 0.0f,     	// 270°
+    0.653f, 0.213f, 0.0f,   	// 292.5°
+    0.6707f, 0.2293f, 0.0f, 	// 315°
+    0.688f, 0.247f, 0.0f,   	// 337.5°
+    //Tinky-Winky: Head (Internal)
+	0.510f, 0.300f, 0.0f,      // 0°
+	0.521f, 0.348f, 0.0f,      // 22.5°
+	0.536f, 0.364f, 0.0f,      // 45°
+	0.552f, 0.378f, 0.0f,      // 67.5°
+	0.600f, 0.390f, 0.0f,      // 90°
+	0.647f, 0.378f, 0.0f,      // 112.5°
+	0.664f, 0.364f, 0.0f,      // 135°
+	0.678f, 0.348f, 0.0f,      // 157.5°
+	0.690f, 0.300f, 0.0f,      // 180°
+	0.678f, 0.252f, 0.0f,      // 202.5°
+	0.664f, 0.236f, 0.0f,      // 225°
+	0.647f, 0.222f, 0.0f,      // 247.5°
+	0.600f, 0.210f, 0.0f,      // 270°
+	0.552f, 0.222f, 0.0f,      // 292.5°
+	0.536f, 0.236f, 0.0f,      // 315°
+	0.521f, 0.252f, 0.0f,      // 337.5°
+	//Tinky-Winky: Body
+	0.50f, 0.20f, 0.0f,			// Top-left
+	0.55f, 0.23f, 0.0f,
+	0.65f, 0.23f, 0.0f,
+	0.70f, 0.20f, 0.0f,			// Top-right
+	0.70f, -0.20f, 0.0f,		// Bottom-right
+	0.50f, -0.20f, 0.0f,		// Bottom-left		
+	//Tinky-Winky: Body (Internal)
+	0.55f, 0.0f, 0.0f,			// Top-left
+	0.65f, 0.0f, 0.0f,			// Top-right
+	0.65f, -0.10f, 0.0f,		// Bottom-right
+	0.55f, -0.10f, 0.0f,		// Bottom-left	
+	//Tinky-Winky: Arm (Left)
+	0.45f, 0.18f, 0.0f,			// Top-left
+	0.50f, 0.20f, 0.0f,			// Top-right
+	0.50f, 0.15f, 0.0f,
+	0.49f, -0.20f, 0.0f,		// Bottom-right
+	0.44f, -0.20f, 0.0f,		// Bottom-left
+	//Tinky-Winky: Arm (Right)
+	0.70f, 0.15, 0.0f,
+	0.70f, 0.20f, 0.0f,			// Top-left
+	0.75f, 0.18f, 0.0f,			// Top-right
+	0.76f, -0.20f, 0.0f,		// Bottom-right
+	0.71f, -0.20f, 0.0f,		// Bottom-left
+	//Tinky-Winky: Leg (Left)
+	0.50f, -0.20f, 0.0f,		// Top-left
+	0.60f, -0.20f, 0.0f,		// Top-right
+	0.58f, -0.50f, 0.0f,		// Bottom-right
+	0.48f, -0.50f, 0.0f,		// Bottom-left
+	//Tinky-Winky: Leg (Right)
+	0.60f, -0.20f, 0.0f,		// Top-left
+	0.70f, -0.20f, 0.0f,		// Top-right
+	0.72f, -0.50f, 0.0f,		// Bottom-right
+	0.62f, -0.50f, 0.0f,		// Bottom-left
+	
+	//Sun Baby
+	0.98f, 0.6786f, 0.0f,      // 0°
+	0.9629f, 0.7540f, 0.0f,    // 22.5°
+	0.9381f, 0.7801f, 0.0f,    // 45°
+	0.9037f, 0.8029f, 0.0f,    // 67.5°
+	0.8271f, 0.8214f, 0.0f,    // 90°
+	0.7529f, 0.8029f, 0.0f,    // 112.5°
+	0.7253f, 0.7801f, 0.0f,    // 135°
+	0.7014f, 0.7540f, 0.0f,    // 157.5°
+	0.6843f, 0.6786f, 0.0f,    // 180°
+	0.7014f, 0.6026f, 0.0f,    // 202.5°
+	0.7253f, 0.5764f, 0.0f,    // 225°
+	0.7529f, 0.5540f, 0.0f,    // 247.5°
+	0.8271f, 0.5357f, 0.0f,    // 270°
+	0.9137f, 0.5540f, 0.0f,    // 292.5°
+	0.9281f, 0.5764f, 0.0f,    // 315°
+	0.9529f, 0.6026f, 0.0f,    // 337.5°
 };
 
 GLfloat colorsVBO[] = {
@@ -536,6 +629,101 @@ GLfloat colorsVBO[] = {
     0.0f, 1.0f, 0.0f,
     0.0f, 1.0f, 0.0f,
     0.0f, 1.0f, 0.0f,
+    
+    
+	//Tink-Wink: Head (Antenna)
+	0.5f, 0.0f, 1.0f,
+	0.5f, 0.0f, 1.0f,
+	0.5f, 0.0f, 1.0f,
+	0.5f, 0.0f, 1.0f,
+	0.5f, 0.0f, 1.0f,
+	0.5f, 0.0f, 1.0f,
+    //Tink-Winky: Head
+    0.5f, 0.0f, 1.0f,	// 0°
+    0.5f, 0.0f, 1.0f,  	// 22.5°
+    0.5f, 0.0f, 1.0f, 	// 45°
+    0.5f, 0.0f, 1.0f,  	// 67.5°
+    0.5f, 0.0f, 1.0f, 	// 90°
+    0.5f, 0.0f, 1.0f,  	// 112.5°
+    0.5f, 0.0f, 1.0f, 	// 135°
+    0.5f, 0.0f, 1.0f,  	// 157.5°
+    0.5f, 0.0f, 1.0f, 	// 180°
+    0.5f, 0.0f, 1.0f,  	// 202.5°
+    0.5f, 0.0f, 1.0f, 	// 225°
+    0.5f, 0.0f, 1.0f,  	// 247.5°
+    0.5f, 0.0f, 1.0f,   // 270°
+    0.5f, 0.0f, 1.0f,  	// 292.5°
+    0.5f, 0.0f, 1.0f, 	// 315°
+    0.5f, 0.0f, 1.0f,  	// 337.5°
+    //Tink-Winky: Head (Internal)
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    1.0f, 0.89f, 0.70f,
+    //Tink-Winky: Body
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+   	//Tink-Winky: Body (Internal)
+	0.5f, 0.5f, 0.5f,
+	0.5f, 0.5f, 0.5f,
+	0.5f, 0.5f, 0.5f,
+	0.5f, 0.5f, 0.5f,
+    //Tink-Winky: Arm (Left)
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    //Tink-Winky: Body (Right)
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    //Tink-Winky: Leg (Left)
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    //Tink-Winky: Leg (Right)
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    0.5f, 0.0f, 1.0f,
+    
+    //Sun Baby
+    1.0f, 1.0f, 0.0f,	// 0°
+    1.0f, 1.0f, 0.0f,  	// 22.5°
+    1.0f, 1.0f, 0.0f, 	// 45°
+    1.0f, 1.0f, 0.0f,  	// 67.5°
+    1.0f, 1.0f, 0.0f, 	// 90°
+    1.0f, 1.0f, 0.0f,  	// 112.5°
+    1.0f, 1.0f, 0.0f, 	// 135°
+    1.0f, 1.0f, 0.0f,  	// 157.5°
+    1.0f, 1.0f, 0.0f, 	// 180°
+    1.0f, 1.0f, 0.0f,  	// 202.5°
+    1.0f, 1.0f, 0.0f, 	// 225°
+    1.0f, 1.0f, 0.0f,  	// 247.5°
+    1.0f, 1.0f, 0.0f,   // 270°
+    1.0f, 1.0f, 0.0f,  	// 292.5°
+    1.0f, 1.0f, 0.0f, 	// 315°
+    1.0f, 1.0f, 0.0f,  	// 337.5°
 };
 
 void renderVerticesVBO() {
@@ -604,6 +792,19 @@ void renderVerticesVBO() {
     glDrawArrays(GL_POLYGON, 207, 5);		//Draw Dipsy: Arm (Right)
     glDrawArrays(GL_QUADS, 212, 4);			//Draw Dipsy: Leg (Left)
     glDrawArrays(GL_QUADS, 216, 4);			//Draw Dipsy: Leg (Right)
+    
+    glDrawArrays(GL_LINE_LOOP, 220, 3);		//Draw Tink-Winky: Head (Antenna)
+    glDrawArrays(GL_LINE_STRIP, 223, 3);	//Draw Tink-Winky: Head (Antenna)
+	glDrawArrays(GL_POLYGON, 226, 16);		//Draw Tink-Winky: Head
+    glDrawArrays(GL_POLYGON, 242, 16);		//Draw Tink-Winky: Head (Internal)
+    glDrawArrays(GL_POLYGON, 258, 6);		//Draw Tink-Winky: Body
+    glDrawArrays(GL_POLYGON, 264, 4);		//Draw Tink-Winky: Body (Internal)
+    glDrawArrays(GL_POLYGON, 268, 5);		//Draw Tink-Winky: Arm (Left)
+    glDrawArrays(GL_POLYGON, 273, 5);		//Draw Tink-Winky: Arm (Right)
+    glDrawArrays(GL_QUADS, 278, 4);			//Draw Tink-Winky: Leg (Left)
+    glDrawArrays(GL_QUADS, 282, 4);			//Draw Tink-Winky: Leg (Right)
+    
+	glDrawArrays(GL_POLYGON, 286, 16);		//Draw Tink-Winky: Head
 }
 
 void display() {
